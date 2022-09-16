70313b7eed7f341d51a3cf9feb93c4cbcd5e50b5
Gunel <gunelminina@gmail.com>
2022-09-16 23:45:38 +0300

    comment

--- Example002_Zadacha2/Program.cs
+++ Example002_Zadacha2/Program.cs
@@ -1,4 +1,10 @@
-﻿Console.Write("Vvedite chislo A: ");
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.Write("Vvedite chislo A: ");
 int numberA = int.Parse(Console.ReadLine());
 Console.Write("Vvedite chislo B: ");
 int numberB = int.Parse(Console.ReadLine());
