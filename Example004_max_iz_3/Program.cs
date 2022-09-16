2085a354bedd643cd5f403c198ca9b9faf0eb135
Gunel <gunelminina@gmail.com>
2022-09-16 23:16:49 +0300

    add Zadacha 4

---
+++ Example004_max_iz_3/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Vvedite chislo A: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Vvedite chislo B: ");
+int numberB = int.Parse(Console.ReadLine());
+Console.Write("Vvedite chislo C: ");
+int numberC = int.Parse(Console.ReadLine());
+if(numberA > numberB & numberA > numberC)
+{
+    Console.Write("max = ");
+    Console.WriteLine(numberA);
+}
+else if(numberB > numberA & numberB > numberC)
+{
+    Console.Write("max = ");
+    Console.WriteLine(numberB);
+}
+else if(numberC > numberA & numberC > numberB)
+{
+    Console.Write("max = ");
+    Console.WriteLine(numberC);
+}
+else
+{
+    Console.Write("Chisla ravny ");
+}
