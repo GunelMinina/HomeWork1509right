06c769719266a663749fc0d8a9d03781679defda
Gunel <gunelminina@gmail.com>
2022-09-16 23:47:19 +0300

    comment

--- Example004_max_iz_3/Program.cs
+++ Example004_max_iz_3/Program.cs
@@ -1,4 +1,10 @@
-﻿Console.Write("Vvedite chislo A: ");
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.Write("Vvedite chislo A: ");
 int numberA = int.Parse(Console.ReadLine());
 Console.Write("Vvedite chislo B: ");
 int numberB = int.Parse(Console.ReadLine());
