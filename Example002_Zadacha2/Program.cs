8d364fac450721b131228b58906713472f952733
Gunel <gunelminina@gmail.com>
2022-09-16 23:06:09 +0300

    add Zadacha2

---
+++ Example002_Zadacha2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Vvedite chislo A: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Vvedite chislo B: ");
+int numberB = int.Parse(Console.ReadLine());
+if(numberA > numberB)
+{
+    Console.Write("max = ");
+    Console.WriteLine(numberA);
+    Console.Write("min = ");
+    Console.WriteLine(numberB);
+}
+else if(numberB > numberA)
+{
+    Console.Write("max = ");
+    Console.WriteLine(numberB);
+    Console.Write("min = ");
+    Console.WriteLine(numberA);
+}
+else
+{
+    Console.Write("max = min = ");
+    Console.WriteLine(numberA);
+}
